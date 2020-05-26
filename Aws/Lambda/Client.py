573652818076daf98ffe71253a50fc55ca07ddb8
Damian Sloane <damian@capacitorlabs.com>
2020-05-27 03:54:21 +1000

    Work in progress

--- Aws/Lambda/Client.py
+++ Aws/Lambda/Client.py
@@ -21,7 +21,7 @@ class Client(BaseClient):
         """
         super().__init__(credential, region_name)
 
-    def invoke(self, function_name, payload, client_context=None) -> Any:
+    def invoke(self, function_name, payload) -> Any:
         """
         Invoke a lambda function
 
@@ -31,17 +31,13 @@ class Client(BaseClient):
         :param payload: JSON payload
         :type payload: str
 
-        :param client_context: Optional client context (up to 3583 bytes of base64 encoded data)
-        :type client_context: Optional[str]
-
         :return: Return value
         """
         result = self.__client__.invoke(
             FunctionName=function_name,
             InvocationType='RequestResponse',
             LogType='Tail',
-            ClientContext=client_context or '',
-            Payload=payload or ''
+            Payload=payload or '{}'
         )
 
         return result
