5cf2c179cce6133a64cbfc1ff24b4e671c83e717
Damian Sloane <damian@capacitorlabs.com>
2020-05-27 03:34:47 +1000

    Work in progress

---
+++ Aws/Lambda/Client.py
@@ -0,0 +1,48 @@
+from typing import Any, Optional
+
+from Aws.BaseClient import BaseClient
+
+
+class Client(BaseClient):
+    """
+    ECS Client
+    """
+    __client_identifier__ = 'lambda'
+
+    def __init__(self, credential, region_name):
+        """
+        Setup an ECS client
+
+        :param credential: The credential used to authenticate to AWS
+        :type credential: Credential
+
+        :param region_name: Region in which client will operate
+        :type region_name: str
+        """
+        super().__init__(credential, region_name)
+
+    def invoke(self, function_name, payload, client_context=None) -> Any:
+        """
+        Invoke a lambda function
+
+        :param function_name: Name of the function
+        :type function_name: str
+
+        :param payload: JSON payload
+        :type payload: str
+
+        :param client_context: Optional client context (up to 3583 bytes of base64 encoded data)
+        :type client_context: Optional[str]
+
+        :return: Return value
+        """
+        result = self.__client__.invoke(
+            FunctionName=function_name,
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            ClientContext=client_context,
+            Payload=payload
+        )
+
+        return result
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eonx-aws',
-    version='0.0.3',
+    version='0.0.4',
     description='eonx-aws',
     url='git@github.com:eonx-com/eonx-aws.git',
     author='Damian Sloane',
