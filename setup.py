8ba700963a20da802f730cc7d24ee793428af024
Damian Sloane <damian@capacitorlabs.com>
2020-05-27 02:51:20 +1000

    WIP

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='eonx-aws',
-    version='0.0.1',
+    version='0.0.2',
     description='eonx-aws',
     url='git@github.com:eonx-com/eonx-aws.git',
     author='Damian Sloane',
     author_email='damian.sloane@eonx.com',
-    license='unlicense',
+    license='unlicensed',
     packages=['Aws'],
     zip_safe=False,
     install_requires=['boto', 'boto3', 'botocore']
